e62d6d33ba1094b775f0129e6689e1bb1492e879
FalguniKhandelwal23 <125128719+FalguniKhandelwal23@users.noreply.github.com>
2023-03-09 10:13:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Accodion from "./component/accordian/Accordion";
+const App=()=>{
+    return (
+        <>
+            <Accodion/>
+        </>
+    )
+}
+export default App;
---
+++ src/component/accordian/api.js
@@ -0,0 +1,19 @@
+export const questions=[
+    {
+        id:1,
+        question:'what is the virtual DOM?',
+        answer:'DOM stands for document object model.'
+    },
+    {
+        id:2,
+        question:'What is JSX?',
+        answer:'JSX stands for JavaScript XML.'
+    },
+    {
+        id:3,
+        question:'What is an event in React?',
+        answer:'An event is an action that a user or system may trigger, such as pressing a key, a mouse click, etc.'
+    }
+    
+
+]
